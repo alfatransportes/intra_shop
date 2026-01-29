f03fdaf97d7310ecab4771f291b0cb991e6b4444
wendel0007 <wendel0429@hotmail.com>
2026-01-28 22:21:56 -0300

    up

--- website/views.py
+++ website/views.py
@@ -1,21 +1,35 @@
 # website/views.py
+from django.db.models import F, IntegerField, Q, Sum
+from django.db.models.functions import Coalesce
 from django.shortcuts import get_object_or_404, render
 
 from .models import Produto, Tipo
 
 
 def index(request):
-    tipo_id = request.GET.get("tipo")  # /?tipo=1
+    tipo_id = request.GET.get("tipo")
 
     produtos = (
         Produto.objects
         .prefetch_related("imagens")
+        .annotate(
+            reservado=Coalesce(
+                Sum(
+                    "itens_carrinho__quantidade",
+                    filter=Q(itens_carrinho__carrinho__status="ABERTO"),
+                ),
+                0,
+                output_field=IntegerField()
+            ),
+            estoque_calc=F("quantidade") - F("reservado"),
+        )
+        .filter(estoque_calc__gt=0)
     )
 
-    if tipo_id:
-        produtos = produtos.filter(tipo_prod_id=tipo_id)
+    if tipo_id and tipo_id.isdigit():
+        produtos = produtos.filter(tipo_prod_id=int(tipo_id))
 
-    tipo_produtos = Tipo.objects.all()
+    tipo_produtos = Tipo.objects.all().order_by("nome")
 
     return render(
         request,
