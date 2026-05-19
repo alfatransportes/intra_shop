dab6d3394c08bfb162fb83b3d0d83cdfa9fa0a53
wendel0007 <wendel0429@hotmail.com>
2026-05-19 13:19:37 -0300

    correções

--- website/apps.py
+++ website/apps.py
@@ -1,9 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def criar_unidade_matriz(sender, **kwargs):
+    Unidade = sender.get_model("Unidade")
+
+    Unidade.objects.get_or_create(
+        codigo=1,
+        defaults={"nome": "Matriz"}
+    )
 
 
 class WebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "website"
 
     def ready(self):
-        from . import signals
+        post_migrate.connect(criar_unidade_matriz, sender=self)
