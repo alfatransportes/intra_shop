d17f34c0ffc57937d1c477d74ededb3e7d121630
wendel0007 <wendel0429@hotmail.com>
2026-05-19 13:58:06 -0300

    correções

--- intra_shop/settings.py
+++ intra_shop/settings.py
@@ -168,13 +168,11 @@ USE_TZ = True
 
 FORCE_SCRIPT_NAME = "/intra_shop"
 
-# Arquivos Staticos
 STATIC_URL = "/intra_shop/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Arquivos de Media/upload
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/intra_shop/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
